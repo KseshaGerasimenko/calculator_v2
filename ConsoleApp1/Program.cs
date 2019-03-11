5dd42e067daca21b2432de5f8cb8750af4e113fe
PestovaMary <pestovamary09@gmail.com>
2019-03-12 01:29:25 +0700

    Div

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,13 @@ namespace ConsoleApp1
     {
         const int menuSize = 5;
 
-
+        static void division(int A, int B)
+        {
+            Console.Clear();
+            int C = A / B;
+            Console.WriteLine("{0}/{1} = {2}", A, B, C);
+            Console.ReadKey();
+        }
 
         static void Main(string[] args)
         {
